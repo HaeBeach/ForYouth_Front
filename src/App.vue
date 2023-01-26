17ce539dbea18d355a13eb6648ac321fccfacdd9
djeon <daewoong.jeon@kt.com>
2023-01-26 23:45:21 +0900

    feat: layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderCom/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderCom from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    HeaderCom
   }
 }
 </script>
 
 <style>
-#app {
+body {
+  margin: 0px;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
+  margin-top: 10px;
+} */
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="background">
+
+    <div id="logo">
+      <img alt="logo" src="../assets/ForYouthlogo.png">
+    </div>
+
+    <div id="menu">
+      <div class="menuButton">
+        부동산
+      </div>
+      <div class="menuButton">
+        취업
+      </div>
+      <div class="menuButton">
+        게시판
+      </div>
+    </div>
+
+    <div id="profileImage">
+      <img class="profileImage" src="../assets/ForYouthlogo.png">
+    </div>
+    <div id="profile">
+      idExample
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderCom'
+}
+</script>
+<style scoped>
+  #background {
+    width: 1440px;
+    height: 72px;
+    align-items: center;
+    background-color: #8177EE;
+  }
+  #logo {
+    width: 212px;
+    margin-top: 20px;
+    text-align: center;
+    float: left;
+  }
+  #menu {
+    width: 675px;
+    margin-top: 20px;
+    text-align: center;
+    float: left;
+  }
+  #profile {
+    width: 212px;
+    margin-top: 22px;
+    text-align: right;
+    color: #FFFFFF;
+    font-size: 24px;
+    float: right;
+  }
+  #profileImage {
+    width: 32px;
+    height: 32px;
+    margin-top: 20px;
+    padding-left: 8px;
+    padding-right: 8px;
+    text-align: center;
+    float: right;
+  }
+
+  div.menuButton {
+    width: 150px;
+    margin-top: 2px;
+    text-align: center;
+    color: #FFFFFF;
+    font-size: 24px;
+    float: left;
+  }
+  img.profileImage {
+    width: 32px;
+    height: 32px;
+    border-radius: 100%;
+  }
+
+  @media (max-width: 1440px) {
+    #background {
+      margin: 0 auto;
+      width: 100%;
+      min-height: 0;
+      padding-top: 0;
+      position: relative;
+    }
+  }
+</style>
